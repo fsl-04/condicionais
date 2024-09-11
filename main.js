136027f7c4b768ec60b54df0f554d602ad6b397b
Carlos Vieira <112698218+carloeira@users.noreply.github.com>
2024-09-10 21:49:21 -0300

    resolucao 2 e 3

--- main.js
+++ main.js
@@ -11,3 +11,34 @@ function questao1() {
         document.getElementById("resultado1").innerHTML = resultado;
     }
 }
+
+function questao2() {
+    let nome = prompt("Digite seu nome: ").toUpperCase();
+    let sexo = prompt("Digite sua (M)asculino ou (F)eminino: ").toUpperCase();
+    let estadoCivil = prompt("Digite seu estado civil:").toUpperCase();
+    if (sexo === "F" && estadoCivil === "CASADA") {
+        let tempoCasada = prompt("Digite o tempo de casada (anos): ");
+        resultado = 
+        `
+        Nome: ${nome}
+        Sexo: ${sexo}
+        Estado Civil: ${estadoCivil}
+        Tempo de casada: ${tempoCasada} anos
+        `;
+    } else {
+        resultado = 
+        `
+        Nome: ${nome}
+        Sexo: ${sexo}
+        Estado Civil: ${estadoCivil}
+        `;
+    }
+    document.getElementById("resultado2").innerHTML = resultado;
+}
+
+function questao3() {
+    let numero = parseInt(prompt("Digite um número: "));
+    let resultado = (numero % 2 === 0) ? `O número ${numero} é par` : `O número ${numero} é ímpar`;
+    document.getElementById("resultado3").innerHTML = resultado;
+}
+
